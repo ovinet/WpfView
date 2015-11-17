ff29678f28143c4e66ae2025d8be78c72be6931d
bytecode <bytecode0101@yahoo.com>
2015-11-17 21:01:11 +0200

    added superSlow

---
+++ MyWarcraft/Models/Builders/SuperSlowBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWarcraft.Models.Builders
+{
+    class SuperSlowBuilder : AbstractBuilder
+    {
+        public SuperSlowBuilder()
+        {
+            TimeForEachStep = 500;
+            PercentageBuiltAtEachStep = 5;
+        }
+    }
+}
